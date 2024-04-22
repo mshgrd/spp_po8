24dbee4b838e51ba5f021788d1ae220968ee50c9
Yakov Burtik <100514972+xKyPaToPx@users.noreply.github.com>
2024-04-22 14:00:21 +0300

    Kapitonov update lab2z1

--- reports/Kapitonov/2/src/lab2/lab2z1/Program.cs
+++ reports/Kapitonov/2/src/lab2/lab2z1/Program.cs
@@ -13,7 +13,7 @@ var etalon = text[0];
 Console.WriteLine($"Etalon: {etalon}");
 foreach (var s in text.Skip(1))
 { 
-    if (s.Contains(etalon[..(etalon.Length / 2)]))
+    if (s.Contains(etalon[..(etalon.Length / 2 + 1)]))
     {
         Console.WriteLine(s);
     }
