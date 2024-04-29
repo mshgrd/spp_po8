2e42a636f83d5de5aae8e8c1f19cf9ccc8f662ff
ValeriaPJul <ValeriaPJul@users.github.com>
2024-04-29 13:35:23 +0300

    Add lab4

---
+++ reports/Pechko/4/src/Task_3/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Car
+    {
+        public int Id { get; }
+        public string Model { get; }
+
+        public Car(int id, string model)
+        {
+            Id = id;
+            Model = model;
+        }
+    }
+}
