8607f581de5e56dc4f350c73e8a5753a5cac85ef
Yakov Burtik <100014633+FatalOptimist@users.noreply.github.com>
2024-04-29 13:57:09 +0300

    Kapitonov fix lab2z1 + add lab3

---
+++ reports/Kapitonov/3/src/lab3/lab3z1/Triangle.cs
@@ -0,0 +1,65 @@
+﻿namespace lab3z1;
+
+public class Triangle
+{
+    private Triangle _triangle;
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+
+    public Triangle(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public Triangle()
+    {
+        A = 3;
+        B = 4;
+        C = 5;
+    }
+    
+    
+    
+    public double CalcP()
+    {
+        return A + B + C;
+    }
+
+    public double CalcS()
+    {
+        double p = (A + B + C) / 2;
+        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+
+    public override string ToString()
+    {
+        return $"A:{A}, B:{B}, C:{C}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (A == ((Triangle)obj).A && B == ((Triangle)obj).B && C == ((Triangle)obj).C)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool Exists()
+    {
+        if (A < B+C && B < A+C && C < A+B)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ reports/Kapitonov/3/src/lab3/lab3z2/Bus.cs
@@ -0,0 +1,101 @@
+﻿namespace lab3z2;
+
+public class Bus
+{
+    public Bus(string name, int busNumber, int routeNumber, string brand, int year, int mileage, BusStatus status)
+    {
+        Name = name;
+        BusNumber = busNumber;
+        RouteNumber = routeNumber;
+        Brand = brand;
+        Year = year;
+        Mileage = mileage;
+        Status = status;
+    }
+
+    public string Name { get; set; }
+    public int BusNumber { get; set; }
+    public int RouteNumber { get; set; }
+    public string Brand { get; set; }
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+    public BusStatus Status { get; set; }
+    
+    public static List<Bus> Buses = new List<Bus>();
+
+    public static void ShowAllBuses()
+    {
+        foreach (var bus in Buses)
+        {
+            Console.WriteLine(bus);
+        }
+    }
+    public static void ShowAllBusesEnRoute()
+    {
+        foreach (var bus in Buses)
+        {
+            if (bus.Status == BusStatus.EnRoute)
+            {
+                Console.WriteLine(bus);
+            }
+        }
+    }
+    
+    public static void ShowAllBusesOnTrip()
+    {
+        foreach (var bus in Buses)
+        {
+            if (bus.Status == BusStatus.OnTrip)
+            {
+                Console.WriteLine(bus);
+            }
+        }
+    }
+    
+    public static void ShowAllBusesWithRouteNumber(int number)
+    {
+        foreach (var bus in Buses)
+        {
+            if (bus.RouteNumber == number)
+            {
+                Console.WriteLine(bus);
+            }
+        }
+    }
+
+    public static void ShowAllBusesElder10Years()
+    {
+        foreach (var bus in Buses)
+        {
+            if (bus.Year < DateTime.Now.Year - 10)
+            {
+                Console.WriteLine(bus);
+            }
+        }
+    }
+
+    public static void ShowAllBusesWithMileage(int mileage)
+    {
+        foreach (var bus in Buses)
+        {
+            if (bus.Mileage > mileage)
+            {
+                Console.WriteLine(bus);
+            }
+        }
+    }
+    
+    public override string ToString()
+    {
+        return $"Name: {Name}, BusNumber: {BusNumber}, RouteNumber: {RouteNumber}, Brand: {Brand}, Year: {Year}, Mileage: {Mileage}, Status: {Status}";
+    }
+
+}
+
+
+
+public enum BusStatus
+{
+    EnRoute,
+    OnTrip
+}
